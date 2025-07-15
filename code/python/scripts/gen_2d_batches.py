9099913e5f0098d2f1cec406a3036657b84e756c
LBPhysics <zxoqi19@s-cloud.uni-tuebingen.de>
2025-07-15 17:21:30 +0200

    minor changes there will be jobs running on proteus right now for the next couple of days

--- code/python/scripts/gen_2d_batches.py
+++ code/python/scripts/gen_2d_batches.py
@@ -4,11 +4,17 @@ from qspectro2d.config import SCRIPTS_DIR
 # for f in batch_*.slurm; do sbatch "$f"; done
 
 TOTAL_BATCHES = 100  # You can increase/decrease this
-T_DET_MAX = 600.0  # Maximum detection time in fs
+T_DET_MAX = 200.0  # Maximum detection time in fs
 DT = 0.1  # Spacing between t_coh, and of also t_det values in fs
 
 
-def create_batch_script(batch_idx, total_batches, job_dir, t_det_max=600, dt=10):
+def create_batch_script(
+    batch_idx: int,
+    total_batches: int,
+    job_dir: Path,
+    t_det_max: float = 600,
+    dt: float = 10,
+) -> Path:
     """Create a batch script for SLURM to run a specific batch of calculations."""
 
     content = f"""#!/bin/bash
@@ -20,7 +26,7 @@ def create_batch_script(batch_idx, total_batches, job_dir, t_det_max=600, dt=10)
 #SBATCH --mail-user=leopold.bodamer@student.uni-tuebingen.de
 
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=10G # this should be more than enough
+#SBATCH --mem=5G # this should be more than enough
 #SBATCH --time=0-6 # 6 hours
 
 source /home/lbodamer/miniconda3/etc/profile.d/conda.sh
@@ -28,7 +34,7 @@ conda activate master_env
 
 cd /home/lbodamer/Master_thesis/code/python/scripts/
 
-python3 calc_datas.py --batch_idx {batch_idx} --n_batches {total_batches} --t_det_max {t_det_max} --dt {dt}
+python calc_datas.py --simulation_type 2d --batch_idx {batch_idx} --n_batches {total_batches} --t_det_max {t_det_max} --dt {dt}
 """
 
     path = job_dir / f"batch_{batch_idx}.slurm"
@@ -41,9 +47,7 @@ def main():
     # =============================
     # Ensure job_dir is unique by appending a timestamp if it exists
     # =============================
-    base_job_dir = Path(
-        SCRIPTS_DIR / f"jobs_t_det_max{T_DET_MAX:.0f}_dt{DT}_{TOTAL_BATCHES}_b"
-    )
+    base_job_dir = Path(SCRIPTS_DIR / f"td_max{T_DET_MAX:.0f}_dt{DT}_{TOTAL_BATCHES}_b")
     # if it already exists, skip the creation
     if base_job_dir.exists():
         print(f"Job directory {base_job_dir} already exists. Skipping creation.")
